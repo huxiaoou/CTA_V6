58b49b448601be956a5e6b175b03ee6a06d9432d
huxiaoou <516984451@qq.com>
2025-08-20 10:35:32 +0800

    Update dates in run_all.ps1

--- run_all.ps1
+++ run_all.ps1
@@ -2,7 +2,10 @@ Remove-Item E:\Data\Projects\CTA_V6\* -Recurse
 
 $bgn_date_avlb = "20120104"
 $bgn_date_factor = "20140102"
-$bgn_date_sig = "20161229" # must at least 2 days ahead of bgn date
+$bgn_date_qtest = "20150105"
+$bgn_date_sig_fac = $bgn_date_qtest
+$bgn_date_opt = "20161229" # must at least 2 days ahead of bgn date
+$bgn_date_sig_stg = $bgn_date_opt
 $bgn_date = "20170109"
 $stp_date = "20250701"
 
@@ -30,46 +33,46 @@ python main.py --bgn $bgn_date_factor --stp $stp_date factor --fclass VENTROPY
 python main.py --bgn $bgn_date_factor --stp $stp_date factor --fclass AMP
 python main.py --bgn $bgn_date_factor --stp $stp_date factor --fclass LCVR
 
-python main.py --bgn $bgn_date --stp $stp_date ic --fclass BASIS
-python main.py --bgn $bgn_date --stp $stp_date ic --fclass TS
-python main.py --bgn $bgn_date --stp $stp_date ic --fclass RS
-python main.py --bgn $bgn_date --stp $stp_date ic --fclass MTM
-python main.py --bgn $bgn_date --stp $stp_date ic --fclass SKEW
-python main.py --bgn $bgn_date --stp $stp_date ic --fclass KURT
-python main.py --bgn $bgn_date --stp $stp_date ic --fclass LIQUIDITY
-python main.py --bgn $bgn_date --stp $stp_date ic --fclass SIZE
-python main.py --bgn $bgn_date --stp $stp_date ic --fclass CTP
-python main.py --bgn $bgn_date --stp $stp_date ic --fclass CVP
-python main.py --bgn $bgn_date --stp $stp_date ic --fclass MPH
-python main.py --bgn $bgn_date --stp $stp_date ic --fclass VAL
-python main.py --bgn $bgn_date --stp $stp_date ic --fclass ACR
-python main.py --bgn $bgn_date --stp $stp_date ic --fclass REOC
-python main.py --bgn $bgn_date --stp $stp_date ic --fclass NPLS
-python main.py --bgn $bgn_date --stp $stp_date ic --fclass VENTROPY
-python main.py --bgn $bgn_date --stp $stp_date ic --fclass AMP
-python main.py --bgn $bgn_date --stp $stp_date ic --fclass LCVR
+python main.py --bgn $bgn_date_qtest --stp $stp_date ic --fclass BASIS
+python main.py --bgn $bgn_date_qtest --stp $stp_date ic --fclass TS
+python main.py --bgn $bgn_date_qtest --stp $stp_date ic --fclass RS
+python main.py --bgn $bgn_date_qtest --stp $stp_date ic --fclass MTM
+python main.py --bgn $bgn_date_qtest --stp $stp_date ic --fclass SKEW
+python main.py --bgn $bgn_date_qtest --stp $stp_date ic --fclass KURT
+python main.py --bgn $bgn_date_qtest --stp $stp_date ic --fclass LIQUIDITY
+python main.py --bgn $bgn_date_qtest --stp $stp_date ic --fclass SIZE
+python main.py --bgn $bgn_date_qtest --stp $stp_date ic --fclass CTP
+python main.py --bgn $bgn_date_qtest --stp $stp_date ic --fclass CVP
+python main.py --bgn $bgn_date_qtest --stp $stp_date ic --fclass MPH
+python main.py --bgn $bgn_date_qtest --stp $stp_date ic --fclass VAL
+python main.py --bgn $bgn_date_qtest --stp $stp_date ic --fclass ACR
+python main.py --bgn $bgn_date_qtest --stp $stp_date ic --fclass REOC
+python main.py --bgn $bgn_date_qtest --stp $stp_date ic --fclass NPLS
+python main.py --bgn $bgn_date_qtest --stp $stp_date ic --fclass VENTROPY
+python main.py --bgn $bgn_date_qtest --stp $stp_date ic --fclass AMP
+python main.py --bgn $bgn_date_qtest --stp $stp_date ic --fclass LCVR
 
-python main.py --bgn $bgn_date --stp $stp_date vt --fclass BASIS
-python main.py --bgn $bgn_date --stp $stp_date vt --fclass TS
-python main.py --bgn $bgn_date --stp $stp_date vt --fclass RS
-python main.py --bgn $bgn_date --stp $stp_date vt --fclass MTM
-python main.py --bgn $bgn_date --stp $stp_date vt --fclass SKEW
-python main.py --bgn $bgn_date --stp $stp_date vt --fclass KURT
-python main.py --bgn $bgn_date --stp $stp_date vt --fclass LIQUIDITY
-python main.py --bgn $bgn_date --stp $stp_date vt --fclass SIZE
-python main.py --bgn $bgn_date --stp $stp_date vt --fclass CTP
-python main.py --bgn $bgn_date --stp $stp_date vt --fclass CVP
-python main.py --bgn $bgn_date --stp $stp_date vt --fclass MPH
-python main.py --bgn $bgn_date --stp $stp_date vt --fclass VAL
-python main.py --bgn $bgn_date --stp $stp_date vt --fclass ACR
-python main.py --bgn $bgn_date --stp $stp_date vt --fclass REOC
-python main.py --bgn $bgn_date --stp $stp_date vt --fclass NPLS
-python main.py --bgn $bgn_date --stp $stp_date vt --fclass VENTROPY
-python main.py --bgn $bgn_date --stp $stp_date vt --fclass AMP
-python main.py --bgn $bgn_date --stp $stp_date vt --fclass LCVR
+python main.py --bgn $bgn_date_qtest --stp $stp_date vt --fclass BASIS
+python main.py --bgn $bgn_date_qtest --stp $stp_date vt --fclass TS
+python main.py --bgn $bgn_date_qtest --stp $stp_date vt --fclass RS
+python main.py --bgn $bgn_date_qtest --stp $stp_date vt --fclass MTM
+python main.py --bgn $bgn_date_qtest --stp $stp_date vt --fclass SKEW
+python main.py --bgn $bgn_date_qtest --stp $stp_date vt --fclass KURT
+python main.py --bgn $bgn_date_qtest --stp $stp_date vt --fclass LIQUIDITY
+python main.py --bgn $bgn_date_qtest --stp $stp_date vt --fclass SIZE
+python main.py --bgn $bgn_date_qtest --stp $stp_date vt --fclass CTP
+python main.py --bgn $bgn_date_qtest --stp $stp_date vt --fclass CVP
+python main.py --bgn $bgn_date_qtest --stp $stp_date vt --fclass MPH
+python main.py --bgn $bgn_date_qtest --stp $stp_date vt --fclass VAL
+python main.py --bgn $bgn_date_qtest --stp $stp_date vt --fclass ACR
+python main.py --bgn $bgn_date_qtest --stp $stp_date vt --fclass REOC
+python main.py --bgn $bgn_date_qtest --stp $stp_date vt --fclass NPLS
+python main.py --bgn $bgn_date_qtest --stp $stp_date vt --fclass VENTROPY
+python main.py --bgn $bgn_date_qtest --stp $stp_date vt --fclass AMP
+python main.py --bgn $bgn_date_qtest --stp $stp_date vt --fclass LCVR
 
-python main.py --bgn $bgn_date_sig --stp $stp_date signals --type factors
-python main.py --bgn $bgn_date_sig --stp $stp_date optimize
-python main.py --bgn $bgn_date_sig --stp $stp_date signals --type strategies
+python main.py --bgn $bgn_date_sig_fac --stp $stp_date signals --type factors
+python main.py --bgn $bgn_date_opt --stp $stp_date optimize
+python main.py --bgn $bgn_date_sig_stg --stp $stp_date signals --type strategies
 python main.py --bgn $bgn_date --stp $stp_date --nomp simulations --type strategies
 python main.py --bgn $bgn_date --stp $stp_date quick

7ac3e6da5a030291f1350e29e40447bb4f3fdb1e
huxiaoou <516984451@qq.com>
2025-09-10 14:14:12 +0800

    Best score =  3.545. (Sharpe, Calmar) = (2.042, 1.503).
      retMean retStd     hpr retAnnual volAnnual sharpe calmar  score    mdd      mddT       ret  id
    0   0.038  0.295  118.98      9.52      4.66  2.042  1.503  3.545  6.333  20230531  Opn005L1  s0
    2   0.037  0.292  113.81      9.23      4.62  1.996  1.375  3.371  6.712  20230531       NaN  p0
    1   0.036  0.294  108.71      8.94      4.65  1.924  1.261  3.185  7.090  20230531  Cls005L1  s1

--- run_since_stg.ps1
+++ run_since_stg.ps1
@@ -1,5 +1,6 @@
 $proj_dir = "E:\Data\Projects\CTA_V6"
 
+Remove-Item "$proj_dir\optimize\*" -Recurse
 Remove-Item "$proj_dir\signals_strategies\*" -Recurse
 Remove-Item "$proj_dir\simulations\*" -Recurse
 Remove-Item "$proj_dir\evaluations\*" -Recurse
@@ -10,6 +11,7 @@ $bgn_date_sig_stg = $bgn_date_opt
 $bgn_date = "20170103"
 $stp_date = "20250801"
 
+python main.py --bgn $bgn_date_opt --stp $stp_date optimize
 python main.py --bgn $bgn_date_sig_stg --stp $stp_date signals --type strategies
 python main.py --bgn $bgn_date --stp $stp_date --nomp simulations
 python main.py --bgn $bgn_date --stp $stp_date quick

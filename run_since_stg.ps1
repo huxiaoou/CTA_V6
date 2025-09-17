674f3163b22819b1d756c27140b7fe24b019fb84
huxiaoou <516984451@qq.com>
2025-09-17 10:40:39 +0800

    Add parameter -EnableSig to run_factor.ps1

--- run_since_stg.ps1
+++ run_since_stg.ps1
@@ -1,12 +1,9 @@
+param (
+    [Parameter(Mandatory = $false)]
+    [Switch] $EnableSig
+)
+
 $proj_dir = "E:\Data\Projects\CTA_V6"
-
-Remove-Item "$proj_dir\signals_factors\*" -Recurse
-Remove-Item "$proj_dir\optimize\*" -Recurse
-Remove-Item "$proj_dir\signals_strategies\*" -Recurse
-Remove-Item "$proj_dir\simulations\*" -Recurse
-Remove-Item "$proj_dir\evaluations\*" -Recurse
-Remove-Item "$proj_dir\sims_quick\*" -Recurse
-
 $bgn_date_qtest = "20150105"
 $bgn_date_sig_fac = $bgn_date_qtest
 $bgn_date_opt = "20161229" # must at least 2 days ahead of bgn date
@@ -14,7 +11,17 @@ $bgn_date_sig_stg = $bgn_date_opt
 $bgn_date = "20170103"
 $stp_date = "20250801"
 
-python main.py --bgn $bgn_date_sig_fac --stp $stp_date signals --type factors
+if ($EnableSig) {
+    Remove-Item "$proj_dir\signals_factors\*" -Recurse
+    python main.py --bgn $bgn_date_sig_fac --stp $stp_date signals --type factors
+}
+
+Remove-Item "$proj_dir\optimize\*" -Recurse
+Remove-Item "$proj_dir\signals_strategies\*" -Recurse
+Remove-Item "$proj_dir\simulations\*" -Recurse
+Remove-Item "$proj_dir\evaluations\*" -Recurse
+Remove-Item "$proj_dir\sims_quick\*" -Recurse
+
 python main.py --bgn $bgn_date_opt --stp $stp_date optimize
 python main.py --bgn $bgn_date_sig_stg --stp $stp_date signals --type strategies
 python main.py --bgn $bgn_date --stp $stp_date --nomp simulations

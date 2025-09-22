5a74306af3e6f6ab82d7b1cec8882f0c8f4f9ad8
huxiaoou <516984451@qq.com>
2025-09-22 15:52:59 +0800

    Best score =  3.969. (Sharpe, Calmar) = (2.088, 1.881).
      retMean retStd     hpr retAnnual volAnnual sharpe calmar  score    mdd      mddT       ret  id
    0   0.038  0.289  119.40      9.54      4.57  2.088  1.881  3.969  5.070  20190917  Opn005L1  s0
    2   0.036  0.286  110.03      9.01      4.52  1.993  1.627  3.620  5.537  20190917       NaN  p0
    1   0.034  0.291  100.97      8.48      4.61  1.842  1.414  3.256  6.002  20190917  Cls005L1  s1
    
    + Add factor MINORDIF

---
+++ factor_algs/minor.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+from husfort.qcalendar import CCalendar
+from typedefs.typedefFactors import CCfgFactorGrpWin, TFactorNames
+from solutions.factor import CFactorsByInstru
+
+
+class CCfgFactorGrpMINOR(CCfgFactorGrpWin):
+    def __init__(self, **kwargs):
+        super().__init__(factor_class="MINOR", **kwargs)
+
+    @property
+    def factor_names(self) -> TFactorNames:
+        return self.names_vanilla + self.names_res + self.names_diff
+
+
+class CFactorMINOR(CFactorsByInstru):
+    def __init__(self, factor_grp: CCfgFactorGrpMINOR, **kwargs):
+        if not isinstance(factor_grp, CCfgFactorGrpMINOR):
+            raise TypeError("factor_grp must be CCfgFactorGrpMINOR")
+        super().__init__(factor_grp=factor_grp, **kwargs)
+        self.cfg = factor_grp
+
+    def cal_factor_by_instru(self, instru: str, bgn_date: str, stp_date: str, calendar: CCalendar) -> pd.DataFrame:
+        buffer_bgn_date = self.cfg.buffer_bgn_date(bgn_date, calendar)
+        adj_data = self.load_preprocess(
+            instru, bgn_date=buffer_bgn_date, stp_date=stp_date,
+            values=["trade_date", "ticker_major", "return_c_major", "return_c_minor"],
+        )
+        minor, major = "return_c_minor", "return_c_major"
+        for win, name_vanilla, name_res in zip(self.cfg.args.wins, self.cfg.names_vanilla, self.cfg.names_res):
+            minor_avg = adj_data[minor].rolling(window=win, min_periods=int(2 * win / 3)).mean()
+            major_avg = adj_data[major].rolling(window=win, min_periods=int(2 * win / 3)).mean()
+            adj_data[name_vanilla] = minor_avg
+            adj_data[name_res] = major_avg - minor_avg
+        w0, w1 = 240, 60
+        n0, n1 = self.cfg.name_vanilla(w0), self.cfg.name_vanilla(w1)
+        adj_data[self.cfg.name_diff()] = adj_data[n0] * np.power(w0 / w1, 0.5) - adj_data[n1]
+        self.rename_ticker(adj_data)
+        factor_data = self.get_factor_data(adj_data, bgn_date)
+        return factor_data
